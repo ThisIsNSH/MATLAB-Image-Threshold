b5d7f75ad3ae394590588921787f937da53154f8
ThisIsNsh <hadanis.singh@gmail.com>
2020-01-08 16:10:22 +0530

    Files Added

---
+++ README.md
@@ -0,0 +1,11 @@
+# MATLAB-Image-Threshold
+
+### Image Processing
+
+Finding Threshold of a colour image.
+--------
+* I have intensity of RGB of a pixel `255` if it is greater than `128` or set to `0`.
+
+<img src="im1.PNG">  
+
+
---
+++ threshold.m
@@ -0,0 +1,14 @@
+﻿function im = threshold(arr) 
+    for row = 1:size(arr,1)
+        for col = 1:size(arr,2) 
+            for pix = 1:size(arr,3) 
+                if (arr(row,col,pix)<128)
+                    arr(row,col,pix) = 0;
+                else
+                    arr(row,col,pix) = 255;
+                end
+            end
+        end
+    end
+    im = arr;
+end
